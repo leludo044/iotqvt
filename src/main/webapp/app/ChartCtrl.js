eea13236796e6ea4af88b4166be6ef5d982892ca
Amaury LECLERC <amauryleclerc@hotmail.fr>
2015-05-21 20:14:56 +0200

    ajout style css pour webcomponent

--- src/main/webapp/app/ChartCtrl.js
+++ src/main/webapp/app/ChartCtrl.js
@@ -2,62 +2,68 @@ iotqvt.controller('ChartCtrl', ['MesureService', '$scope', '$rootScope', functio
 
 	$scope.mesures =	MesureService.mesures;
 		
-
-
 	$scope.$watchCollection('mesures', function(newMesure,  oldMesure) {
+		console.log("new");
+		console.log(newMesure);
+		console.log("old");
+		console.log(oldMesure);
+		if(oldMesure.length ===0 && newMesure.length > 0){
+			 $scope.chartCtrl.config.series.push({name:newMesure[0].id, data:[]});
+		}
 		for(x in newMesure){
-			 $scope.highchartsNG.series[0].data.push([newMesure[x].date,newMesure[x].temp/1000]);
+		
+			 $scope.chartCtrl.config.series[0].data.push([newMesure[x].date,newMesure[x].temp/1000]);
 		}
 		
 		});
-
 	
-
-    $scope.highchartsNG = {
-        options: {
-            chart: {
-                type: 'line'
-            }
-        },
-        xAxis : {
-			type : 'datetime',
-			dateTimeLabelFormats : {
-				month : '%e. %b',
-				year : '%b'
+		
+		this.capteur = $scope.capteur;
+		
+		this.config =  {
+	        options: {
+	            chart: {
+	                type: 'line'
+	            }
+	        },
+	        xAxis : {
+				type : 'datetime',
+				dateTimeLabelFormats : {
+					month : '%e. %b',
+					year : '%b'
+				},
+				title : {
+					text : 'Date'
+				}
 			},
-			title : {
-				text : 'Date'
-			}
-		},
-		yAxis : {
-			title : {
-				text : 'Temperature (°C)'
+			yAxis : {
+				title : {
+					text : this.capteur.name+' '+this.capteur.unite
+				},
+				plotLines : [ {
+					value : 0,
+					width : 1,
+					color : '#808080'
+				} ]
 			},
-			plotLines : [ {
-				value : 0,
-				width : 1,
-				color : '#808080'
-			} ]
-		},
-		tooltip : {
-			valueSuffix : '°C'
-		},
-		legend : {
-			layout : 'vertical',
-			align : 'right',
-			verticalAlign : 'middle',
-			borderWidth : 0
-		},
-        series:[{
-        	name:"rasp",
-        	data:[]
-        		
-        }],
-        title: {
-            text: 'Temperature'
-        },
-        loading: false
-    }
+			tooltip : {
+				valueSuffix : this.capteur.unite
+			},
+			legend : {
+				layout : 'vertical',
+				align : 'right',
+				verticalAlign : 'middle',
+				borderWidth : 0
+			},
+	        series:[],
+	        title: {
+	            text: '',
+	            style: {
+	                display: 'none'
+	            }
+	        }
+	    };
+	
 
 
 }]);
--- src/main/webapp/app/IOTCtrl.js
+++ src/main/webapp/app/IOTCtrl.js
@@ -1,6 +1,6 @@
 iotqvt.controller('IOTCtrl' , function () {
 
-	this.iots = [{id:'rasp-amaury', capteurs:[{name:"Temperature"}]},{id:'rasp-ludo',capteurs:[]}, {id:'rasp-jerome',capteurs:[{name:"Temperature"},{name:"Bruit"}]}];
+	this.iots = [{id:'rasp-amaury', capteurs:[{name:"Temperature", unite:"°C"}]},{id:'rasp-ludo',capteurs:[]}, {id:'rasp-jerome',capteurs:[{name:"Temperature" , unite:"°C"},{name:"Bruit", unite:"dB"},{name:"Lum", unite:"dB"}]}];
 	this.iotSelect = null;
 
 });

b702a876e05fe3e8727da6285e62d9b6e8b68bbe
Amaury LECLERC <amauryleclerc@hotmail.fr>
2015-05-25 13:26:50 +0200

    ajout getall redis

--- src/main/java/fr/iotqvt/master/infra/MesureEntrepot.java
+++ src/main/java/fr/iotqvt/master/infra/MesureEntrepot.java
@@ -4,12 +4,13 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import redis.clients.jedis.Jedis;
 import fr.iotqvt.master.modele.Mesure;
 
 public class MesureEntrepot {
-private static ArrayList<Mesure> list = new ArrayList<Mesure>();
+//private static ArrayList<Mesure> list = new ArrayList<Mesure>();
 
 
 public static void ajouter(Mesure m){
@@ -23,10 +24,31 @@ public static void ajouter(Mesure m){
 	Jedis jedis = new Jedis(redisURI);
 	jedis.set(String.valueOf(m.getDate()), String.valueOf(m.getTemp()));
 	jedis.close();
-	list.add(m);
+//	list.add(m);
 }
 
 public static List<Mesure> getAll(){
+	ArrayList<Mesure> list = new ArrayList<Mesure>();
+	URI redisURI = null;
+	  try {
+		 redisURI = new URI("redis://:1nJi6I8z7Z8jJNfd@pub-redis-18189.eu-west-1-1.1.ec2.garantiadata.com:18189");
+	} catch (URISyntaxException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	Jedis jedis = new Jedis(redisURI);
+	Set<String> keys = jedis.keys("*");
+	for (String key : keys) {
+		Mesure m = new Mesure();
+		String value = jedis.get(key);
+		m.setId("test");
+		m.setDate(Long.parseLong(key));
+		m.setTemp(Integer.valueOf(value));
+	
+		list.add(m);
+	}
+	jedis.close();
+	
 	return list;
 }
 

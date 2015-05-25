27d0b785e4605b607b70d48539d6063b9d0aa3cb
Amaury LECLERC <amauryleclerc@hotmail.fr>
2015-05-25 13:01:44 +0200

    ajout tomcat 8 et jedis

--- src/main/java/fr/iotqvt/master/infra/MesureEntrepot.java
+++ src/main/java/fr/iotqvt/master/infra/MesureEntrepot.java
@@ -3,6 +3,7 @@ package fr.iotqvt.master.infra;
 import java.util.ArrayList;
 import java.util.List;
 
+import redis.clients.jedis.Jedis;
 import fr.iotqvt.master.modele.Mesure;
 
 public class MesureEntrepot {
@@ -10,6 +11,9 @@ private static ArrayList<Mesure> list = new ArrayList<Mesure>();
 
 
 public static void ajouter(Mesure m){
+	Jedis jedis = new Jedis(":1nJi6I8z7Z8jJNfd@pub-redis-18189.eu-west-1-1.1.ec2.garantiadata.com:18189");
+	jedis.set(String.valueOf(m.getDate()), String.valueOf(m.getTemp()));
+	jedis.close();
 	list.add(m);
 }
 

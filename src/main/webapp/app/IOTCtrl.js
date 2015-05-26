0fe4ab396db7f7595dae5d6b6c020b811acaf55f
leludo044 <leludo044@gmail.com>
2015-05-26 18:48:18 +0200

    Inscription du capteur de température pour rasp-ludo

--- src/main/webapp/app/IOTCtrl.js
+++ src/main/webapp/app/IOTCtrl.js
@@ -1,6 +1,6 @@
 iotqvt.controller('IOTCtrl' , function () {
 
-	this.iots = [{id:'rasp-amaury', capteurs:[{name:"Temperature", unite:"°C"}]},{id:'rasp-ludo',capteurs:[]}, {id:'rasp-jerome',capteurs:[{name:"Temperature" , unite:"°C"},{name:"Bruit", unite:"dB"},{name:"Luminosite", unite:"Lux"}]}];
+	this.iots = [{id:'rasp-amaury', capteurs:[{name:"Temperature", unite:"°C"}]},{id:'rasp-ludo',capteurs:[{name:"Temperature", unite:"°C"}]}, {id:'rasp-jerome',capteurs:[{name:"Temperature" , unite:"°C"},{name:"Bruit", unite:"dB"},{name:"Luminosite", unite:"Lux"}]}];
 	this.iotSelect = null;
 
 });

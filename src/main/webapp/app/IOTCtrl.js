1d4531a543ef93d889ac46fdf10eb7a82108eaa5
Amaury LECLERC <amauryleclerc@hotmail.fr>
2015-05-30 11:23:11 +0200

    ajout couleur ihm

--- src/main/webapp/app/IOTCtrl.js
+++ src/main/webapp/app/IOTCtrl.js
@@ -1,6 +1,6 @@
 iotqvt.controller('IOTCtrl' , function () {
 
-	this.iots = [{id:'rasp-amaury', capteurs:[{name:"Temperature", unite:"°C"}]},{id:'rasp-ludo',capteurs:[{name:"Temperature", unite:"°C"}]}, {id:'rasp-jerome',capteurs:[{name:"Temperature" , unite:"°C"},{name:"Bruit", unite:"dB"},{name:"Luminosite", unite:"Lux"}]}];
+	this.iots = [{id:'rasp-amaury', statut:"rouge", capteurs:[{name:"Temperature", couleur:"bleu", statut:"vert", unite:"°C"}]},{id:'rasp-ludo', statut:"rouge", capteurs:[{name:"Temperature" , unite:"°C", couleur:"bleu", statut:"vert"}]}, {id:'rasp-jerome', statut:"vert", capteurs:[{name:"Temperature" , couleur:"bleu",  statut:"rouge", unite:"°C"},{name:"Bruit",  couleur:"orange",  statut:"rouge", unite:"dB"},{name:"Luminosite",  couleur:"violet",  statut:"vert", unite:"Lux"}]}];
 	this.iotSelect = null;
 
 });

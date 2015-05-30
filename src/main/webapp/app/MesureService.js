c7d973ef8519b9c822f44fd24057c0ca4a22b998
Amaury LECLERC <amauryleclerc@hotmail.fr>
2015-05-30 12:20:54 +0200

    modification modele ihm

--- src/main/webapp/app/MesureService.js
+++ src/main/webapp/app/MesureService.js
@@ -1,74 +1,71 @@
 'use strict';
 
-iotqvt.service('MesureService', function(WebSocketService,WebServiceFactory, _) {
-	
-	
-	var mesures = [];
-	var chartData = [];
-	var last = {
-		temp : null,
-		date : null
-	};
-	var max = {
-		temp : null,
-		date : null
-	};
-	var min = {
-		temp : null,
-		date : null
-	};
-	var first = {
-		temp : null,
-		date : null
-	};
-	WebServiceFactory.onReceiveData(function(data) {
-		var x;
-		for(x in data){
-			mesures.push(data[x]);
-			chartData.push([ data[x].date, data[x].temp / 1000 ]);
-		}
+iotqvt.service('MesureService',
+		function(WebSocketService, WebServiceFactory, _) {
 
-		_.assign(last, _.max(mesures, function(mesure) {
-			return mesure.date;
-		}));
-		_.assign(first, _.min(mesures, function(mesure) {
-			return mesure.date;
-		}));
-		_.assign(max, _.max(mesures, function(mesure) {
-			return mesure.temp;
-		}));
-		_.assign(min, _.min(mesures, function(mesure) {
-			return mesure.temp;
-		}));
+			var mesures = [];
+			var chartData = [];
+			var last = {
+				valeur : null,
+				date : null
+			};
+			var max = {
+				valeur : null,
+				date : null
+			};
+			var min = {
+				valeur : null,
+				date : null
+			};
+			var first = {
+				valeur : null,
+				date : null
+			};
+			WebServiceFactory.onReceiveData(function(data) {
+				var x;
+				for (x in data) {
+					mesures.push(data[x]);
+					chartData.push([ data[x].date, data[x].valeur ]);
+				}
 
+				_.assign(last, _.max(mesures, function(mesure) {
+					return mesure.date;
+				}));
+				_.assign(first, _.min(mesures, function(mesure) {
+					return mesure.date;
+				}));
+				_.assign(max, _.max(mesures, function(mesure) {
+					return mesure.valeur;
+				}));
+				_.assign(min, _.min(mesures, function(mesure) {
+					return mesure.valeur;
+				}));
 
-	});
-	
-	
-	WebSocketService.onReceiveData(function(data) {
-		mesures.push(data);
-		chartData.push([ data.date, data.temp / 1000 ]);
-		_.assign(last, _.max(mesures, function(mesure) {
-			return mesure.date;
-		}));
-		_.assign(first, _.min(mesures, function(mesure) {
-			return mesure.date;
-		}));
-		_.assign(max, _.max(mesures, function(mesure) {
-			return mesure.temp;
-		}));
-		_.assign(min, _.min(mesures, function(mesure) {
-			return mesure.temp;
-		}));
+			});
 
+			WebSocketService.onReceiveData(function(data) {
+				mesures.push(data);
+				chartData.push([ data.date, data.valeur ]);
+				_.assign(last, _.max(mesures, function(mesure) {
+					return mesure.date;
+				}));
+				_.assign(first, _.min(mesures, function(mesure) {
+					return mesure.date;
+				}));
+				_.assign(max, _.max(mesures, function(mesure) {
+					return mesure.valeur;
+				}));
+				_.assign(min, _.min(mesures, function(mesure) {
+					return mesure.valeur;
+				}));
 
-	});
-	return {
-		mesures : mesures,
-		chartData : chartData,
-		last : last,
-		first : first,
-		min : min,
-		max : max
-	};
-});
+			});
+			return {
+				mesures : mesures,
+				chartData : chartData,
+				last : last,
+				first : first,
+				min : min,
+				max : max
+			};
+		});

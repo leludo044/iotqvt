f02b3ffc1cf82db596e50f101de627da1e557337
Amaury LECLERC <amauryleclerc@hotmail.fr>
2015-05-31 18:11:00 +0200

    correction

--- src/main/webapp/app/IOTService.js
+++ src/main/webapp/app/IOTService.js
@@ -15,7 +15,7 @@ iotqvt.service('IOTService', ['$http',
     	   $http.get(url).success( function(data){
     			var x;
 				for (x in data) {
-					data[x].capteur[0].couleur = "bleu";
+					data[x].capteurs[0].couleur = "bleu";
 					iots.push(data[x]);
 				}
     	   }

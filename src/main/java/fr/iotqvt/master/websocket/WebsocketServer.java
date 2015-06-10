10772e0a5846ec375e927dccca9c265c5350d0b2
Ludovic <leludo044@gmail.com>
2015-06-10 14:33:06 +0200

    Spécialisation du message concernant la mesure

--- src/main/java/fr/iotqvt/master/websocket/WebsocketServer.java
+++ src/main/java/fr/iotqvt/master/websocket/WebsocketServer.java
@@ -1,6 +1,5 @@
 package fr.iotqvt.master.websocket;
 
-
 import java.sql.SQLException;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
@@ -21,9 +20,10 @@ import fr.iotqvt.master.modele.jdbc.Jdbc;
 import fr.iotqvt.master.modele.metier.IOT;
 import fr.iotqvt.master.modele.metier.Mesure;
 
-
 @ServerEndpoint(value = "/websocket/mesure")
 public class WebsocketServer {
+
+	private static final String MESSAGE_MESURE_PREFIXE = "{\"type\":\"mesure\"";
 
 	private static final Set<Session> sessions = new CopyOnWriteArraySet<Session>();
 	private Session session;
@@ -35,65 +35,67 @@ public class WebsocketServer {
 		System.out.println("start");
 	}
 
-
 	@OnClose
 	public void end() {
 		sessions.remove(session);
 		System.out.println("end");
 	}
 
-
 	@OnMessage
 	public void incoming(String json) {
-		System.out.println("onMessage"+json);
-		try{
-			Gson gson = new Gson();
-			MesureMessage msg = gson.fromJson(json, MesureMessage.class);
-			Mesure m = msg.getMesure();
-			if(m.getCapteur() != null){
-				 try {
+		System.out.println("onMessage" + json);
+		try {
+			if (json.startsWith(MESSAGE_MESURE_PREFIXE)) {
+				Gson gson = new Gson();
+				MesureMessage msg = gson.fromJson(json, MesureMessage.class);
+				Mesure m = msg.getMesure();
+				if (m.getCapteur() != null) {
+					try {
 						Jdbc.getInstance().connecter();
 					} catch (ClassNotFoundException | SQLException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
-				IOTDAO iotDao = new IOTDAO();
-				CapteurDAO capteurDao = new CapteurDAO();
-				MesureDAO mesuredao = new MesureDAO();
-				iotDao.create(new IOT(m.getCapteur().getIot()));
-				capteurDao.create(m.getCapteur());
-				mesuredao.create(m);
-				broadcastText(json);
-				 try {
+					IOTDAO iotDao = new IOTDAO();
+					CapteurDAO capteurDao = new CapteurDAO();
+					MesureDAO mesuredao = new MesureDAO();
+					iotDao.create(new IOT(m.getCapteur().getIot()));
+					capteurDao.create(m.getCapteur());
+					mesuredao.create(m);
+					broadcastText(json);
+					try {
 						Jdbc.getInstance().deconnecter();
-					} catch ( SQLException e1) {
+					} catch (SQLException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
+				}
 			}
-	
-		
-		}catch(Exception e){
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
+
 	@OnError
 	public void onError(Throwable t) throws Throwable {
 		System.out.println("error");
 		System.out.println(t.getMessage());
 		t.printStackTrace();
 	}
-	public void broadcast(Mesure m){
+
+	public void broadcast(Mesure m) {
 		Gson gson = new Gson();
 		String json = gson.toJson(m);
-		
-		for(Session uneSession : sessions){
+
+		for (Session uneSession : sessions) {
 			uneSession.getAsyncRemote().sendText(json);
 		}
 	}
-	public void broadcastText(String json){
-		for(Session uneSession : sessions){
+
+	public void broadcastText(String json) {
+		for (Session uneSession : sessions) {
 			uneSession.getAsyncRemote().sendText(json);
 		}
 	}
